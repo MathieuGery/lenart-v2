823efa0049c0e4a36359e985e57610bea1f5c7b2
Mathieu <mathieu.gery@epitech.eu>
2026-03-18 22:57:35 +0100

    Update default app url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
-    appUrl: process.env.NUXT_APP_URL ?? 'http://localhost:3000',
+    appUrl: process.env.NUXT_APP_URL ?? 'https://v2.len-art.fr',
     public: {
       photoPriceCents: process.env.PHOTO_PRICE_CENTS ?? '500'
     }
